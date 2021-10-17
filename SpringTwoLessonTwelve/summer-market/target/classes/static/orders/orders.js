907b769b1cbd566d164aae7657f837c70c02fe3f
Sergey <1@1.com>
2021-10-17 17:31:29 +0300

    assfaf12

---
+++ SpringTwoLessonTwelve/summer-market/target/classes/static/orders/orders.js
@@ -0,0 +1,19 @@
+angular.module('app').controller('ordersController', function ($scope, $http, $localStorage, $location) {
+    const contextPath = 'http://localhost:8189/summer';
+
+    $scope.loadOrders = function () {
+        $http({
+            url: contextPath + '/api/v1/orders',
+            method: 'GET'
+        }).then(function (response) {
+            $scope.orders = response.data;
+            console.log($scope.orders);
+        });
+    }
+
+    $scope.pay = function (orderId) {
+        $location.path('/create_order/' + orderId);
+    }
+
+    $scope.loadOrders();
+});
